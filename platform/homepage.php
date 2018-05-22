09118630ffad86d59a875d683f5c169106a3ed46
cosinechen <tree7035612@yahoo.com.tw>
2018-05-22 23:52:49 +0800

    sidebar

--- platform/homepage.php
+++ platform/homepage.php
@@ -4,7 +4,7 @@
   
   <!-- Top header -->
   <header class="w3-container w3-xlarge">
-    <p class="w3-left">Search</p>
+    <p class="w3-left">Hi</p>
     <p class="w3-right">
       <i class="fa fa-shopping-cart w3-margin-right"></i>
       <i class="fa fa-search"></i>
