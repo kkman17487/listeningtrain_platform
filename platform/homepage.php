15846f5b8efa9fb26f4621d990fdc47d1c93a2f7
cosinechen <tree7035612@yahoo.com.tw>
2018-05-24 11:38:10 +0800

    delete search

--- platform/homepage.php
+++ platform/homepage.php
@@ -5,10 +5,6 @@
   <!-- Top header -->
   <header class="w3-container w3-xlarge">
     <p class="w3-left">Hi</p>
-    <p class="w3-right">
-      <i class="fa fa-shopping-cart w3-margin-right"></i>
-      <i class="fa fa-search"></i>
-    </p>
   </header>
 
   <!-- Image header -->
