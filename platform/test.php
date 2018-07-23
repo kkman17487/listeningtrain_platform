8b6a43e8ab83d5314786a2a2afb73c0fd2a74f05
RecorderZero <ian03121997@gmail.com>
2018-07-23 10:40:03 +0800

    exam.php

---
+++ platform/test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<?php include('sidebar.php'); ?>
+  
+  <!-- Top header -->
+  <header class="w3-container w3-xlarge">
+    <p class="w3-left">Hi</p>
+  </header>
+
+  <!-- Image header -->
+  <div class="w3-display-container w3-container">
+    <img src="../picture/test1.jpg" alt="Jeans" style="width:100%">
+    <div class="w3-display-topleft w3-text-white" style="padding:24px 48px">
+      <h1 class="w3-jumbo w3-hide-small">HOME PAGE</h1>
+      <h1 class="w3-hide-small">Welcome</h1>
+      <p><a href="" class="w3-button w3-black w3-padding-large w3-large">Start</a></p>
+    </div>
+  </div>
+  <!-- End page content -->
+</div>
+
+</body>
+</html>
