49ea89b2d3618cef580eab3cc67477c7ea06274e
RecorderZero <ian03121997@gmail.com>
2018-07-23 10:43:27 +0800

    exam.php

--- platform/sidebar.php
+++ platform/sidebar.php
@@ -21,7 +21,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
   <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
     <a href="homepage.php" class="w3-bar-item w3-button">首頁</a>
     <a href="training.php" class="w3-bar-item w3-button">訓練模式</a>
-	<a onclick="myAccFunc()" href="test.php" class="w3-button w3-block w3-white w3-left-align" id="myBtn">測驗模式<i class="fa fa-caret-down"></i>
+	<a onclick="myAccFunc()" href="javascript:void(0)" class="w3-button w3-block w3-white w3-left-align" id="myBtn">測驗模式<i class="fa fa-caret-down"></i>
     </a>
     <div id="demoAcc" class="w3-bar-block w3-hide w3-padding-large w3-medium">
       <a href="" class="w3-bar-item w3-button">hi</a>
