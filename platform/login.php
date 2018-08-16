0094548c076466bd8121ad0df9b756f01c8ded36
RecorderZero <ian03121997@gmail.com>
2018-08-16 12:16:55 +0800

    login

--- platform/login.php
+++ platform/login.php
@@ -9,7 +9,7 @@ if($_SESSION['login'] == "yes")
 if(isset($_POST['password']) && isset($_POST['username']))
 {
 include('connect_to_sql.php');
-$admin = $con->query("select * from admin where username LIKE '%$_POST["username"]%'");
+$admin = $con->query("select * from admin where username LIKE '$_POST["username"]'");
 $rs = mysqli_fetch_assoc($admin);
   if(is_null($admin) || $_POST['password'] != $rs['password'])
   {
