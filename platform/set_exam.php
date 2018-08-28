b5497263f9ea0cb84900ace1f9f8ec1700a75ca6
RecorderZero <ian03121997@gmail.com>
2018-08-29 02:53:23 +0800

    set_exam

--- platform/set_exam.php
+++ platform/set_exam.php
@@ -120,7 +120,7 @@ if(isset($_POST['name']) && isset($_POST['question']))
     $question .= $value.",";
   }
   $quesion = substr($question, 0, -1)
-  $date = date('m-d-Y h:i:s a', time());
+  $date = date('m-d-Y h:i:s', time());
   $con->query("UPDATE exam SET question='$question',name='$name',recent_edit_time='$date' WHERE id='$ID'");
 }
 if(!isset($_GET['ID'])){
