29b0e464f11aebfe66e00999e3350511ed114cd6
cosinechen <tree7035612@yahoo.com.tw>
2018-08-29 15:38:09 +0800

    delete unnecessary files

--- platform/answerstate.php
+++ platform/answerstate.php
@@ -6,9 +6,9 @@
 <?php include('backendsidebar.php'); ?>
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 <h1 class="sub-header">作答情況</h1>
-          <div class="table-responsive">
-            <table class="table table-striped">
-              <thead>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
                 <tr>
                   <th>#</th>
                   <th>Header</th>
@@ -16,8 +16,8 @@
                   <th>Header</th>
                   <th>Header</th>
                 </tr>
-              </thead>
-              <tbody>
+            </thead>
+            <tbody>
                 <tr>
                   <td>1,001</td>
                   <td>Lorem</td>
@@ -32,9 +32,34 @@
                   <td>adipiscing</td>
                   <td>elit</td>
                 </tr>
-              </tbody>
-            </table>
-          </div>
+            </tbody>
+        </table>
+    </div>
+		  
+<h1 class="page-header">圖表</h1>
+    <div class="row placeholders">
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+    </div> 
+	
 </div>
 </body>
 </html>	
