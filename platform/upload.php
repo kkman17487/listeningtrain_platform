53fb9db8487266b442997496f283a104195d6f19
cosinechen <tree7035612@yahoo.com.tw>
2018-08-30 11:44:33 +0800

    upload

--- platform/upload.php
+++ platform/upload.php
@@ -7,7 +7,7 @@ for ($i = 0; $i < $fileCount; $i++) {
   if ($_FILES['my_file']['error'][$i] === UPLOAD_ERR_OK){
     echo '檔案名稱: ' . $_FILES['my_file']['name'][$i] . '<br/>';
     echo '檔案類型: ' . $_FILES['my_file']['type'][$i] . '<br/>';
-    echo '檔案大小: ' . ($_FILES['my_file']['size'][$i] / 1024000) . ' MB<br/>';
+    echo '檔案大小: ' . ($_FILES['my_file']['size'][$i] / 1024) . ' KB<br/>';
     echo '暫存名稱: ' . $_FILES['my_file']['tmp_name'][$i] . '<br/>';
 
     # 檢查檔案是否已經存在
