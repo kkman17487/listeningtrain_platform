d549beb0b65218f21f81243e2e31ec9855546501
RecorderZero <ian03121997@gmail.com>
2018-09-20 11:02:16 +0800

    exam

--- platform/exambysystem.php
+++ platform/exambysystem.php
@@ -115,7 +115,7 @@ elseif(isset($_GET['ID']) && !isset($_GET['checkanswer']))
 
               for($j = 0;$j < 4;$j++){
 
-              echo '<td><input type="radio" id="answer'.$i.'" name="answer'.$i.'" value="'.$answer[$j][$j].'">'.$answer[$j][$j].'<img height="100" width="100" src="'.$answer[$j][$j+1].'"></td>';
+              echo '<td><input type="radio" id="answer'.$i.'" name="answer'.$i.'" value="'.$answer[$j][0].'">'.$answer[$j][0].'<img height="100" width="100" src="'.$answer[$j][1].'"></td>';
               //print_r($rs);
             }
           echo '
