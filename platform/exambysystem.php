db56b0bb8b37487ce9de2c2b590ec4ac805a25d0
RecorderZero <ian03121997@gmail.com>
2018-09-23 20:20:51 +0800

    exam

--- platform/exambysystem.php
+++ platform/exambysystem.php
@@ -91,7 +91,7 @@ elseif ($_GET['no'] > 0)
     }
     echo '<div class="w3-row">';
     $next = $_GET['no'] +1;
-    if($_GET['no'] == (sizeof($_SESSION['data'])-1))
+    if($_GET['no'] == (mysqli_num_rows($_SESSION['data'])-1))
       if(isset($_GET['ID']))
         echo '<form name="answer" method="post" action="exambysystem.php?ID='.$_GET['ID'].'&checkanswer=true">';
       elseif(isset($_GET['number']))
@@ -137,8 +137,8 @@ elseif ($_GET['no'] > 0)
             }
           echo '
         </tr>
-        </table>'.sizeof($_SESSION['data']);
-    if($_GET['no'] == (sizeof($_SESSION['data'])-1))
+        </table>';
+    if($_GET['no'] == (mysqli_num_rows($_SESSION['data'])-1))
       echo '<br><input type="submit" name="submit" value="提交" align="center"></form></div>';
     else
       echo '<br><input type="submit" name="submit" value="下一題" align="center"></form></div>';
