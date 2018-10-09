d0e0bee6c7e5d8398a39f87a1b2f24a1ca7bbe0e
RecorderZero <ian03121997@gmail.com>
2018-10-09 18:48:18 +0800

    test position

---
+++ platform/test0.php
@@ -0,0 +1,32 @@
+<script>
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+//建立一個物件儲存滑鼠目前的x,y座標
+let mouse = {
+    x : 0,
+    y : 0,
+}
+
+//加入監聽器
+window.addEventListener('mousemove',(event) => {
+  //在這裡把滑鼠座標寫到物件mouse中
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+})
+function run()
+{
+  document.getElementById('x').value = mouse.x;
+	document.getElementById('y').value = mouse.y;
+  var t=setTimeout("run()",1);
+}
+</script>
+<body>
+	<canvas id="myCanvas"></canvas>
+	<form>
+		<input type="text" id="x" name="x">
+		<input type="text" id="y" name="y">
+	</form>
+</body>
