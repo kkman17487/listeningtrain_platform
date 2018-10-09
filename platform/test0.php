96a6220271ad0a773a3f3494df88229977508586
RecorderZero <ian03121997@gmail.com>
2018-10-09 23:22:59 +0800

    test position

--- platform/test0.php
+++ platform/test0.php
@@ -18,14 +18,17 @@ div {
 
 <div onmousemove="myFunction(event)" onmouseout="clearCoor()">
 
-	<!--<div class="w3-col l3 s6">
+	<div class="w3-col">
 		<div class="w3-container">
 			<div class="w3-display-container">-->
 				<audio id='<?php echo $rs[audio_id]?>'>
 					<source src="<?php echo $rs[sound_src]?>" type="audio/mp3" />
 					<embed height="100" width="100" src="<?php echo $rs[sound_src]?>" />
 				</audio>
-				<img src="<?php echo $rs[pic_src]?>" height="100%" width="100%" />
+				<img src="<?php echo $rs[pic_src]?>" height="100px" width="100px" />
+			</div>
+		</div>
+	</div>
 
 </div>
 
