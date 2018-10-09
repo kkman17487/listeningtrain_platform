7d357b5acb8283fe5f1d63684fac1bed406059a4
RecorderZero <ian03121997@gmail.com>
2018-10-09 23:32:22 +0800

    test position

--- platform/test0.php
+++ platform/test0.php
@@ -22,15 +22,22 @@ include('sidebar.php');
 
 	<div class="w3-col">
 		<div class="w3-container">
-			<div class="w3-display-container">-->
+			<div class="w3-display-container">
 				<audio id='<?php echo $rs[audio_id]?>'>
 					<source src="<?php echo $rs[sound_src]?>" type="audio/mp3" />
 					<embed height="100" width="100" src="<?php echo $rs[sound_src]?>" />
 				</audio>
-				<img src="<?php echo $rs[pic_src]?>" height="100px" width="100px" />
+				<img src="<?php echo $rs[pic_src]?>" height="100%" width="100%" />
+				<?php
+				//if()
+					//echo '<span class="w3-tag w3-display-topleft">New</span>'?>
+				<div class="w3-display-middle w3-display-hover">
+					<button class="w3-button w3-black " onclick="document.getElementById('<?php echo $rs[audio_id]?>').play(); return false;">Play</button>
+				</div>
 			</div>
+			<p align = 'center'><?php echo $rs[name]?></p>
 		</div>
-	</div>
+</div>
 
 </div>
 <p id="demo"></p>
