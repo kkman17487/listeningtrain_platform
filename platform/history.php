9a0e90df5c944ead877ea0b7881c3eaab2e0c2a3
cosinechen <tree7035612@yahoo.com.tw>
2018-10-23 01:56:33 +0800

    history

--- platform/history.php
+++ platform/history.php
@@ -8,7 +8,20 @@
 	array("y" => 0, "label" => "Friday"),
 	array("y" => 20, "label" => "Saturday")
 	//array("y" => correct rate, "label" => "Reaction Time")
-);
+	);
+
+	$dataPoints2 = array( 
+	array("y" => 3373.64, "label" => "Germany" ),
+	array("y" => 2435.94, "label" => "France" ),
+	array("y" => 1842.55, "label" => "China" ),
+	array("y" => 1828.55, "label" => "Russia" ),
+	array("y" => 1039.99, "label" => "Switzerland" ),
+	array("y" => 765.215, "label" => "Japan" ),
+	array("y" => 612.453, "label" => "Netherlands" )
+	);
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -33,6 +46,26 @@ var chart = new CanvasJS.Chart("chartContainer", {
 chart.render();
 
 }
+
+window.onload = function() {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: "Gold Reserves"
+	},
+	axisY: {
+		title: "Gold Reserves (in tonnes)"
+	},
+	data: [{
+		type: "column",
+		yValueFormatString: "#,##0.## tonnes",
+		dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+
 </script>
 </head>
 
