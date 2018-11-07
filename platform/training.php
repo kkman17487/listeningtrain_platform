af2b30e1751b04b50a662635cde9b79dec814941
cosinechen <tree7035612@yahoo.com.tw>
2018-11-08 00:01:04 +0800

    modify

--- platform/training.php
+++ platform/training.php
@@ -25,11 +25,6 @@ include('sidebar.php');
       else
         echo '<p class="w3-left">教材'.$_GET['ID'].':'.$name.'</p>';
     ?>
-
-    <!--<p class="w3-right">
-      <i class="fa fa-shopping-cart w3-margin-right"></i>
-      <i class="fa fa-search"></i>
-    </p>-->
   </header>
   <body>
   <?php
