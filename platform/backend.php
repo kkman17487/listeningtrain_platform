ed827922f2e9df5cfb0e0eab383c3457cfdf77c6
cosinechen <tree7035612@yahoo.com.tw>
2018-11-07 17:03:47 +0800

    backend

--- platform/backend.php
+++ platform/backend.php
@@ -5,13 +5,14 @@
 <style>
 li {
 	font-weight:bold;
+	font-family:微軟正黑體;
 }
 </style>
 </head>
 <body>
 <?php include('backendsidebar.php'); ?>
 
-<p align=center><img src="../picture/background.jpg" alt="首頁" style="width:50%"></p>
+<p align=center><img src="../picture/background.jpg" alt="首頁" style="width:55%"></p>
 
 </body>
 </html>
