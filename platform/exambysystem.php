e21e0087b6a947c4b301f04077412f0eec79c4b0
cosinechen <tree7035612@yahoo.com.tw>
2018-11-08 00:04:45 +0800

    header

--- platform/exambysystem.php
+++ platform/exambysystem.php
@@ -46,7 +46,7 @@ elseif(isset($_GET['checkanswer']))
 <?php include('sidebar.php'); ?>
   <!-- Top header -->
   <header class="w3-container w3-xlarge">
-    <!--<p class="w3-left">系統出題</p>-->
+    <p class="w3-left">.</p>
   </header>
   <!-- Image header -->
   <?php
