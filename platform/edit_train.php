c4da79abf49fa3e2301c6469e359f0e0bda1db53
cosinechen <tree7035612@yahoo.com.tw>
2018-11-08 01:24:31 +0800

    modify

--- platform/edit_train.php
+++ platform/edit_train.php
@@ -139,7 +139,7 @@ include('backendheader.php'); ?>
 <body>
 <?php include('backendsidebar.php'); ?>
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-<h1 class="sub-header">出題模式</h1>
+<h1 class="sub-header">編輯訓練教材</h1>
 題庫選擇
 <?php
 if(!isset($_GET['ID'])){
--- platform/set_exam.php
+++ platform/set_exam.php
@@ -139,8 +139,7 @@ include('backendheader.php'); ?>
 <body>
 <?php include('backendsidebar.php'); ?>
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-<h1 class="sub-header">出題模式</h1>
-題庫選擇
+<h1 class="sub-header">題庫選擇</h1>
 <?php
 if(!isset($_GET['ID'])){
 $data = $con->query("select * from exam");
