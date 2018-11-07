200a8d01388df1214dabd5395d85d5ae17200e05
cosinechen <tree7035612@yahoo.com.tw>
2018-11-08 03:03:52 +0800

    proportion

--- platform/training.php
+++ platform/training.php
@@ -77,7 +77,7 @@ include('sidebar.php');
             <source src="<?php echo $rs[sound_src]?>" type="audio/mp3" />
             <embed height="100" width="100" src="<?php echo $rs[sound_src]?>" />
           </audio>
-          <img src="<?php echo $rs[pic_src]?>" height="100%" width="100%" />
+          <img src="<?php echo $rs[pic_src]?>" height="80%" width="80%" />
           <?php
           //if()
             //echo '<span class="w3-tag w3-display-topleft">New</span>'?>
