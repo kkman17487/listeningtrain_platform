b5f02cfad100e3b6e95d4157b8b28f818639f348
cosinechen <tree7035612@yahoo.com.tw>
2018-11-08 11:42:11 +0800

    history

--- platform/history.php
+++ platform/history.php
@@ -128,13 +128,13 @@ function toggleDataSeries(e){
     </div>
 
 <?php	
-	for($i=1;$i<mysqli_num_rows($dbdata);$i++)
+	/*for($i=1;$i<mysqli_num_rows($dbdata);$i++)
 	{ 
 		$labelrs=mysqli_fetch_row($dbdata);
-	}
+	}*/
 	
 	$dataPoints = array(
-	array("y" => "echo $labelrs[0]", "label" => "echo $labelrs[3]")
+	array("y" => echo $rs[0], "label" => echo $rs[3])
 	/*array("y" => 90, "label" => "2018/10/24 02:45")
 	array("y" => 15, "label" => "Monday"),
 	array("y" => correct rate, "label" => "Reaction Time")*/
