d22088065fdaad9a606af555a4de6086a8ecd365
cosinechen <tree7035612@yahoo.com.tw>
2018-11-14 16:03:20 +0800

    Update platform/history.php

--- platform/history.php
+++ platform/history.php
@@ -17,8 +17,8 @@ $dataPoints2 = array(
 			array("x" => 27, "y" => 250),
 			array("x" => 22, "y" => 160)
 		);
-/*include('connect_to_sql.php');
-$dbdata = $con->query("select * from history");
+include('connect_to_sql.php');
+/*$dbdata = $con->query("select * from history");
 	for($i=0;$i<mysqli_num_rows($dbdata);$i++)
 	{
 		$labelrs=mysqli_fetch_row($dbdata);
