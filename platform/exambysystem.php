1326301512842215d09fbfc1afe75109003b09a6
RecorderZero <ian03121997@gmail.com>
2018-11-16 08:47:27 +0800

    exam

--- platform/exambysystem.php
+++ platform/exambysystem.php
@@ -130,9 +130,9 @@ elseif(isset($_GET['checkanswer']))
     }
     elseif(isset($_GET['number']))
       echo '
-      <form name="answer" method="post" action="exambysystem.php?number='.$_GET['number'].'&no='.$next.'">';
+      <form name="answer_sheet" id="answer_sheet" method="post" action="exambysystem.php?number='.$_GET['number'].'&no='.$next.'">';
     elseif(isset($_GET['ID']))
-      echo '<form name="answer" method="post" action="exambysystem.php?ID='.$_GET['ID'].'&no='.$next.'">';
+      echo '<form name="answer_sheet" id="answer_sheet" method="post" action="exambysystem.php?ID='.$_GET['ID'].'&no='.$next.'">';
     echo '<input type="text" name="time" id="time" hidden>';
     $rs = $_SESSION['read'][$_GET['no']];
     echo '
