4008e96697a8f6e1cdcb054706217429a597b2fa
RecorderZero <ian03121997@gmail.com>
2018-11-16 09:26:04 +0800

    exam

--- platform/exambysystem.php
+++ platform/exambysystem.php
@@ -160,7 +160,7 @@ elseif(isset($_GET['checkanswer']))
                 echo '<div class="w3-col l6 s6">
                   <div class="w3-container">
                     <div class="w3-display-container">';
-              echo '<input type="radio" id="answer[]" name="answer" value="'.$answer[$j][0].'" disabled>'.$answer[$j][0].'<br><img height="40%" width="90%" src="'.$answer[$j][1].'"></div></div></div>';
+              echo '<input type="radio" id="answer'.$j.'" name="answer" value="'.$answer[$j][0].'" disabled>'.$answer[$j][0].'<br><img height="40%" width="90%" src="'.$answer[$j][1].'"></div></div></div>';
               //print_r($rs);
               if($j%2==1)echo '</div>';
             }
@@ -262,8 +262,10 @@ function timer()
 }
 function enable()
 {
-  for(var i = 0;i < 4;i++)
-    document.getElementById("answer[i]").disabled = false;
+  document.getElementById("answer0").disabled = false;
+  document.getElementById("answer1").disabled = false;
+  document.getElementById("answer2").disabled = false;
+  document.getElementById("answer3").disabled = false;
 }
 </script>
 </body>
