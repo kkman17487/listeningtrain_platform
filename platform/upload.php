5ba9486695e75283ca58de882521000eed4493e5
RecorderZero <ian03121997@gmail.com>
2018-11-27 15:24:46 +0800

    upload

--- platform/upload.php
+++ platform/upload.php
@@ -51,7 +51,7 @@ for ($i = 0; $i < $fileCount; $i++) {
 	else {
 		echo 'error';
 	}
-  echo
+  echo $pic_src.' '.$sound_pic;
 	$res = $con->query("INSERT INTO `data` (`pic_src`,`sound_src`,`tag`,`name`,`frequency`,`waveform`,`created_time`,`audio_id`) VALUES('$pic_src','$sound_pic','','$_POST[ChineseName]','','',CURRENT_TIMESTAMP,'$_POST[EnglishName]')");
 	if (!$res) {
 	die('Invalid query: ' . mysqli_error($con));
