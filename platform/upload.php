b3f0b81d42fe4b3cb1ff25aa6c3fa3fa4d92e265
RecorderZero <ian03121997@gmail.com>
2018-12-04 21:57:20 +0800

    edit_data

--- platform/upload.php
+++ platform/upload.php
@@ -61,11 +61,12 @@ foreach ($_POST['formcategory'] as $value)
 {
   $category .= $value.";";
 }
-foreach ($_POST['category'] as $value)
+if(isset($_POST['category']))
 {
-  $category .= $value.";";
+  $category .= $_POST['category'];
+  if(substr($category,-1) == ";")
+    $category = substr($category,0,-1);
 }
-$category = substr($category,0,-1);
 $res = $con->query("INSERT INTO `data` (`pic_src`,`sound_src`,`category`,`name`,`frequency`,`waveform`,`created_time`,`audio_id`) VALUES('$pic_src','$sound_src','$category','$_POST[ChineseName]','$frequency','$waveform',CURRENT_TIMESTAMP,'$_POST[EnglishName]')");
 if (!$res) {
 die('Invalid query: ' . mysqli_error($con));
--- platform/upload_edit.php
+++ platform/upload_edit.php
@@ -65,11 +65,12 @@ foreach ($_POST['formcategory'] as $value)
 {
   $category .= $value.";";
 }
-foreach ($_POST['category'] as $value)
+if(isset($_POST['category']))
 {
-  $category .= $value.";";
+  $category .= $_POST['category'];
+  if(substr($category,-1) == ";")
+    $category = substr($category,0,-1);
 }
-$category = substr($category,0,-1);
 if(isset($sound_src))
   $sql .= " sound_src = '$sound_src',";
 if(isset($pic_src))
