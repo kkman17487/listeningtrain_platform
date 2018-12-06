cb299aaed46d4e80a5e00f178f9144ac42a6c7b7
RecorderZero <ian03121997@gmail.com>
2018-12-06 11:13:31 +0800

    train

--- platform/training_all.php
+++ platform/training_all.php
@@ -57,7 +57,7 @@ include('sidebar.php');
           //if()
             //echo '<span class="w3-tag w3-display-topleft">New</span>'?>
           <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black " onclick="sound('<?php echo $rs['audio_id'];?>'); return false;">Play</button>
+            <button class="w3-button w3-black " onclick="sound('<?php echo $rs['audio_id'];?>');">Play</button>
           </div>
         </div>
         <p align = 'center'><?php echo $rs[name]?></p>
@@ -72,13 +72,13 @@ include('sidebar.php');
 var previous_play;
 function sound(next_play)
 {
-  alert(next_play);
   if(typeof previous_play !== 'undefined')
   {
     document.getElementById('previous_play').pause();
     document.getElementById('previous_play').currentTime = 0;
   }
   document.getElementById('next_play').play();
+  return false;
   previous_play = next_play;
 }
 </script>
